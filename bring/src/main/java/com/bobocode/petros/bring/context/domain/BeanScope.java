eef21811bef1638b28c9dca6a644dfbc7cc1d990
Kirill Kotenok <kirill.kotenok@data-ox.com>
2022-07-19 12:18:58 +0300

    Removed inner enum BeanScope and create it separately. Added simple javadoc to BeanScope class.

---
+++ bring/src/main/java/com/bobocode/petros/bring/context/domain/BeanScope.java
@@ -0,0 +1,11 @@
+package com.bobocode.petros.bring.context.domain;
+
+
+/**
+ * The constants of current enumerated class describe the various
+ * of bean creation scope. That impact to method of creating beans.
+ */
+public enum BeanScope {
+    SINGLETON,
+    PROTOTYPE
+}
