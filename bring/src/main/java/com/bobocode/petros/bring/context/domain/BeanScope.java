bb96d97f0f3656ed0913d897cd5e0ea2143e744e
Kirill Kotenok <kirill.kotenok@data-ox.com>
2022-07-19 16:18:14 +0300

    BeanScope javadoc fix

--- bring/src/main/java/com/bobocode/petros/bring/context/domain/BeanScope.java
+++ bring/src/main/java/com/bobocode/petros/bring/context/domain/BeanScope.java
@@ -2,7 +2,7 @@ package com.bobocode.petros.bring.context.domain;
 
 
 /**
- * The constants of current enumerated class describe the various
+ * The constants of current enumeration describe the various
  * of bean creation scope. That impact to method of creating beans.
  */
 public enum BeanScope {
