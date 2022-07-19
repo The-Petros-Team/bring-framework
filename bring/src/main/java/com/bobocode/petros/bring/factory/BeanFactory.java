c7f705b1a4e0bfceefb2fad2a6b7a9397c317d49
Kirill Kotenok <kirill.kotenok@data-ox.com>
2022-07-19 12:24:10 +0300

    Removed useless methods. Fixed method name for getAllBeanReferences method. Added simple javadoc.

--- bring/src/main/java/com/bobocode/petros/bring/factory/BeanFactory.java
+++ bring/src/main/java/com/bobocode/petros/bring/factory/BeanFactory.java
@@ -5,13 +5,25 @@ import com.bobocode.petros.bring.context.domain.BeanReference;
 
 import java.util.Map;
 
+/**
+ * Basic interface that provide contract information about
+ * how beanFactory implementation should work.
+ */
 public interface BeanFactory {
 
-    Map<String, BeanReference> getAllBeanReference();
+    /**
+     * This method using to get all beans. It`s creating beans undercover from bean definition
+     * and put it into a map.
+     * @return Map where key parameter it`s {@link String} bean name and value parameter
+     * it`s {@link BeanReference}
+     */
+    Map<String, BeanReference> getAllBeanReferences();
 
-    BeanReference createBeanReference(String beanName);
 
-    BeanReference createBeanReference(Class<?> beanType);
-
+    /**
+     * This method using to get NEW bean by {@link BeanDefinition}
+     * @param beanDefinition
+     * @return {@link BeanReference}
+     */
     BeanReference createBeanReference(BeanDefinition beanDefinition);
 }
