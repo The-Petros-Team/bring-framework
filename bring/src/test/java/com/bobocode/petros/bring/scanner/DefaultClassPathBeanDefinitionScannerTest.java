1efcc4bcca61b58f12027768bf304fdbb7338fd4
Your Name <test@example.com>
2022-07-27 17:14:52 +0200

    fixes after review

--- bring/src/test/java/com/bobocode/petros/bring/scanner/DefaultClassPathBeanDefinitionScannerTest.java
+++ bring/src/test/java/com/bobocode/petros/bring/scanner/DefaultClassPathBeanDefinitionScannerTest.java
@@ -11,7 +11,9 @@ import org.reflections.scanners.Scanners;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @ExtendWith(MockitoExtension.class)
 public class DefaultClassPathBeanDefinitionScannerTest {
