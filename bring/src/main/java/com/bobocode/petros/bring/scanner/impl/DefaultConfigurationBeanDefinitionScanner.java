a65fd2c8c6a8badac5411d35efeabc49d4be00ed
v.romantsev <v.romantsev@ocado.com>
2022-07-31 19:50:29 +0300

    * excluded annotations from scanning process * moved several methods to ScanningUtils class * renamed DependencyPair -> Dependency

--- bring/src/main/java/com/bobocode/petros/bring/scanner/impl/DefaultConfigurationBeanDefinitionScanner.java
+++ bring/src/main/java/com/bobocode/petros/bring/scanner/impl/DefaultConfigurationBeanDefinitionScanner.java
@@ -1,22 +1,21 @@
 package com.bobocode.petros.bring.scanner.impl;
 
-import com.bobocode.petros.bring.annotation.*;
+import com.bobocode.petros.bring.annotation.Bean;
 import com.bobocode.petros.bring.context.domain.BeanDefinition;
-import com.bobocode.petros.bring.context.domain.DependencyPair;
-import com.bobocode.petros.bring.exception.IllegalBeanDefinitionStateException;
+import com.bobocode.petros.bring.context.domain.Dependency;
 import com.bobocode.petros.bring.exception.InvalidModifierException;
 import com.bobocode.petros.bring.scanner.ConfigurationBeanDefinitionScanner;
 import com.bobocode.petros.bring.utils.BeanNameUtils;
+import com.bobocode.petros.bring.utils.ScanningUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static com.bobocode.petros.bring.context.domain.BeanScope.SINGLETON;
 import static com.bobocode.petros.bring.context.domain.BeanScope.getScopeAsString;
-import static com.bobocode.petros.bring.exception.ExceptionMessage.*;
+import static com.bobocode.petros.bring.exception.ExceptionMessage.METHOD_MUST_NOT_BE_PRIVATE;
 
 /**
  * Implementation of {@link ConfigurationBeanDefinitionScanner}.
@@ -36,7 +35,7 @@ public class DefaultConfigurationBeanDefinitionScanner implements ConfigurationB
             return Collections.emptyList();
         }
         final List<BeanDefinition> beanDefinitions = new ArrayList<>();
-        final Set<Class<?>> configurationClasses = getConfigurationClasses(classes);
+        final Set<Class<?>> configurationClasses = ScanningUtils.getConfigurationClasses(classes);
         log.debug("Found {} configuration classes", configurationClasses.size());
         for (final Class<?> clazz : configurationClasses) {
             final Method[] methods = clazz.getDeclaredMethods();
@@ -49,9 +48,11 @@ public class DefaultConfigurationBeanDefinitionScanner implements ConfigurationB
                     final Class<?> returnType = method.getReturnType();
                     if (returnType.isInterface()) {
                         beanDefinition.setInterface(true);
-                        final List<Class<?>> implementations = findImplementations(classes, returnType);
-                        log.debug("Found {} implementations for '{}'", implementations.size(), returnType.getName());
-                        checkImplementations(implementations.size(), returnType.getName());
+                        final List<Class<?>> implementations = ScanningUtils.findImplementations(classes, returnType);
+                        final String typeName = returnType.getName();
+                        final int size = implementations.size();
+                        log.debug("Found {} implementations for '{}'", size, typeName);
+                        ScanningUtils.checkImplementations(size, typeName);
                         final Map<String, Object> beanDefinitionImplementations = beanDefinition.getImplementations();
                         implementations.forEach(impl -> beanDefinitionImplementations.put(method.getName(), impl));
                     }
@@ -59,24 +60,23 @@ public class DefaultConfigurationBeanDefinitionScanner implements ConfigurationB
                     final Class<?>[] parameterTypes = method.getParameterTypes();
                     log.debug("Found {} parameter types for {} method", parameterTypes.length, method.getName());
                     if (parameterTypes.length >= 1) {
-                        final Map<String, DependencyPair> dependencies = beanDefinition.getDependencies();
+                        final Map<String, Dependency> dependencies = beanDefinition.getDependencies();
                         for (final Class<?> parameterType : parameterTypes) {
-                            final DependencyPair pair = new DependencyPair();
+                            final Dependency dependency = new Dependency();
                             Class<?> implementation = parameterType;
-                            pair.setImplementation(implementation);
+                            dependency.setImplementation(implementation);
                             if (parameterType.isInterface()) {
-                                final List<Class<?>> implementations = findImplementations(classes, parameterType);
+                                final List<Class<?>> implementations = ScanningUtils.findImplementations(classes, parameterType);
+                                implementation = implementations.iterator().next();
                                 log.debug(
                                         "Found {} implementation(s) for resolved dependency '{}' of bean definition: {}",
                                         implementations.size(), parameterType.getName(), beanDefinition.getBeanName()
                                 );
-                                checkImplementations(implementations.size(), parameterType.getName());
-                                final Iterator<Class<?>> iterator = implementations.iterator();
-                                implementation = iterator.next();
-                                pair.setInterfaceClass(parameterType);
-                                pair.setImplementation(implementation);
+                                ScanningUtils.checkImplementations(implementations.size(), parameterType.getName());
+                                dependency.setInterfaceClass(parameterType);
+                                dependency.setImplementation(implementation);
                             }
-                            final boolean isRegisteredAsComponent = isRegisteredAsComponent(implementation);
+                            final boolean isRegisteredAsComponent = ScanningUtils.isRegisteredAsComponent(implementation);
                             String parameterTypeName;
                             if (isRegisteredAsComponent) {
                                 log.debug("Class '{}' is registered as a component", implementation.getName());
@@ -84,12 +84,12 @@ public class DefaultConfigurationBeanDefinitionScanner implements ConfigurationB
                             } else {
                                 final boolean isRegisteredAsBeanCandidate =
                                         isRegisteredInCurrentClass(methods, implementation)
-                                                || isRegisteredGlobally(configurationClasses, implementation);
-                                checkBeanCandidate(implementation, isRegisteredAsBeanCandidate);
+                                                || ScanningUtils.isRegisteredGlobally(configurationClasses, implementation);
+                                ScanningUtils.checkBeanCandidate(implementation, isRegisteredAsBeanCandidate);
                                 log.debug("Class '{}' is registered in Java configuration", implementation.getName());
                                 parameterTypeName = method.getName();
                             }
-                            dependencies.put(parameterTypeName, pair);
+                            dependencies.put(parameterTypeName, dependency);
                         }
                     }
                     beanDefinitions.add(beanDefinition);
@@ -97,69 +97,6 @@ public class DefaultConfigurationBeanDefinitionScanner implements ConfigurationB
             }
         }
         return beanDefinitions;
-    }
-
-    /**
-     * Checks whether a class is registered as a bean candidate. Throws an exception if condition mismatched.
-     *
-     * @param implementation              class that is not registered properly
-     * @param isRegisteredAsBeanCandidate condition to check
-     */
-    private void checkBeanCandidate(final Class<?> implementation, final boolean isRegisteredAsBeanCandidate) {
-        if (!isRegisteredAsBeanCandidate) {
-            throw new IllegalArgumentException(String.format(CLASS_IS_NOT_REGISTERED_AS_BEAN_CANDIDATE, implementation.getName()));
-        }
-    }
-
-    /**
-     * Checks whether an interface has no more that a single implementation and throws an exception if yes.
-     *
-     * @param implementations number of implementations
-     * @param type            interface name
-     */
-    private void checkImplementations(final int implementations, final String type) {
-        if (implementations != 1) {
-            throw new IllegalBeanDefinitionStateException(String.format(INTERFACE_HAS_MORE_THEN_ONE_IMPLEMENTATION, type));
-        }
-    }
-
-    /**
-     * Returns implementations for a given interface.
-     *
-     * @param classes     classes to scan
-     * @param anInterface an interface which implementations should be found
-     * @return list of implementations
-     */
-    private List<Class<?>> findImplementations(final Set<Class<?>> classes, final Class<?> anInterface) {
-        return classes.stream()
-                .filter(clazz -> !clazz.isInterface())
-                .filter(anInterface::isAssignableFrom)
-                .toList();
-    }
-
-    /**
-     * Filters out configuration classes from a set of all classes from a root package.
-     *
-     * @param classes classes to filter
-     * @return set of configuration classes
-     */
-    private Set<Class<?>> getConfigurationClasses(final Set<Class<?>> classes) {
-        return classes.stream()
-                .filter(clazz -> clazz.isAnnotationPresent(Configuration.class))
-                .collect(Collectors.toSet());
-    }
-
-    /**
-     * Checks whether a class is registered in any of the given configuration classes.
-     *
-     * @param classes classes to filter
-     * @param type    class that is supposed to be found among the given set of classes
-     * @return true if class is registered in one of a given configuration classes or false otherwise
-     */
-    private boolean isRegisteredGlobally(final Set<Class<?>> classes, final Class<?> type) {
-        return classes.stream()
-                .flatMap(clazz -> Arrays.stream(clazz.getDeclaredMethods()))
-                .anyMatch(method -> method.getReturnType().isAssignableFrom(type));
     }
 
     /**
@@ -171,18 +108,6 @@ public class DefaultConfigurationBeanDefinitionScanner implements ConfigurationB
      */
     private boolean isRegisteredInCurrentClass(final Method[] methods, final Class<?> type) {
         return Arrays.stream(methods).anyMatch(method -> method.getReturnType().isAssignableFrom(type));
-    }
-
-    /**
-     * Checks whether a class is annotated with one of the following annotations: @Component, @Service, @Repository.
-     *
-     * @param type class that is under a check
-     * @return true if the class is annotated via one of the given annotations or false otherwise
-     */
-    private boolean isRegisteredAsComponent(final Class<?> type) {
-        return type.isAnnotationPresent(Component.class)
-                || type.isAnnotationPresent(Service.class)
-                || type.isAnnotationPresent(Repository.class);
     }
 
     /**
